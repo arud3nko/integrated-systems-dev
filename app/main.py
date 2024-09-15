fd01fa745836bbf564c4732dfe9534c635c9a20c
arud3nko <edm.rud3nko@yandex.ru>
2024-09-15 12:34:25 +0300

    #1 - Updated entry point, docker-compose.yml & requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 """This module contains entry point & app configuration"""
 
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.api.lab1 import lab1_asteroids_router
 
-app = FastAPI()
+from app.lab1.neowise import NeowiseAPIClient
 
 
-@app.get("/")
-async def _():
-    return "hello"
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    """
+        Contains lifespan events.
+    """
+
+    neowise_api_client = NeowiseAPIClient("https://data.nasa.gov/resource/2vr3-k9wn.json")
+
+    yield {
+        "neowise_api_client": neowise_api_client
+    }
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(lab1_asteroids_router, prefix="/lab1")
